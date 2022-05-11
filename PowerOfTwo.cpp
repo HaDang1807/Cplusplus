9b0b824a20dd9e29249bd97bfd533ee16f4bc686
HaDang1807 <93642489+HaDang1807@users.noreply.github.com>
2022-05-11 16:23:32 +0800

    Get started to C++

---
+++ PowerOfTwo.cpp
@@ -0,0 +1,41 @@
+/*
+* File: PowerOfTwo.cpp
+* --------------------
+* This program generates a list of the powers of
+* two up to an exponent limit entered by the user.
+*/
+
+#include <iostream>
+using namespace std;
+
+/* Function prototypes */
+
+int raiseToPower(int n, int k);
+
+/* Main function */
+
+int main() {
+    int limit;
+    cout << "This program lists powers of two." << endl;
+    cout << "Enter exponent limit: ";
+    cin >> limit;
+    for (int i = 0; i <= limit; i++) {
+        cout << "2 to the " << i <<  " = "
+        << raiseToPower(2, i) << endl;
+    }
+    return 0;
+}
+/* 
+* Function: raiseToPower 
+* Usage: int p = raisetoPower(n, k);
+* ----------------------------------
+* Returns the integer n raised to the kth power.
+*/
+
+int raiseToPower(int n, int k) {
+    int result = 1;
+    for (int i = 1; i <= k; i++) {
+        result *= n;
+    }
+    return result;
+}
